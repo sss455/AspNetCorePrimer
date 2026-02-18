b6a32b2f34b81c636719b1723ce67ac2f6fc0329
sss455 <m_ch_n_m1255@yahoo.co.jp>
2026-02-18 17:57:33 +0900

    2026/02/18作業分

--- src/ch04/SampleCFModelMvc/Models/Person.cs
+++ src/ch04/SampleCFModelMvc/Models/Person.cs
@@ -1,11 +1,15 @@
-﻿namespace SampleCFModelMvc.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleCFModelMvc.Models;
 
 public class Person
 {
     public int Id { get; set; }
 
+    [Display(Name="氏名")]
     public string Name { get; set; } = null!;
 
+    [Display(Name="年齢")]
     public int Age { get; set; }
 
 
